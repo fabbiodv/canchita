bb1c794ed1e44e864bf136f8950de7c3ceabbd09
Fabio Mazzarella <fabiomazzarella97@hotmail.com>
2024-12-13 01:02:37 -0300

    fix types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
 import { useState } from "react";
 import { es } from "date-fns/locale";
 import { format } from 'date-fns'
